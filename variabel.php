bbd6a9113a04e42e1bc56b8be9de904722d99c72
Fransiskus24 <93038614+Fransiskus24@users.noreply.github.com>
2021-10-23 20:12:36 +0700

    Add files via upload

---
+++ variabel.php
@@ -0,0 +1,13 @@
+<?php 
+$nama = "fransiskus xaverius kaley";
+$umur = "21";
+$asalperguruantinggi ='Universitas Gajayana Malang';
+$semester = 5;
+$jurusan = "Sistem Informasi";
+
+      echo "hay Perkenalkan nama saya ".$nama.",umur saya ".$umur."
+      tahun. sekarang saya kulia di ".
+      $asalperguruantinggi.", semester ".$semester." jurusan ".$jurusan;
+
+
+?>
